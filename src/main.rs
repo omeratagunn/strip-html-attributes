db368c24e938863b2e5201932a66b914ab230bb3
omeratagun <omratagn@gmail.com>
2023-01-12 20:14:50 +0100

    output time elapsed and in colors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::time::Instant;
 use clap::Parser;
+use colored::Colorize;
 use strip_html_attributes::{
     find_and_replace_recursively, DelimiterSchema,
     Delimiters,
@@ -18,6 +20,7 @@ fn main() {
     // todo filter by only these files //
     let _supported_file_types = ["js", "jsx", "ts", "tsx", "html"];
 
+    let start_time = Instant::now();
     let args = Args::parse();
     let is_operator = '=';
 
@@ -41,4 +44,6 @@ fn main() {
         ],
     };
     find_and_replace_recursively(&args.folder, is_operator, attribute, &delimiters);
+    let output = format!("{}", "Completed successfully!. \nTime taken for operation: ".green().bold());
+    println!("{output}{}",  start_time.elapsed().as_millis() as u32)
 }
